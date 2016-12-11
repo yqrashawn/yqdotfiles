d72c7adacd637f498ebd6585e5e5be9d5581d895
yqrashawn <namy.19@gmail.com>
2016-12-12 06:22:28 +0800

    update package

--- .emacs.d/elpa/open-junk-file-20160912.1859/open-junk-file-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "open-junk-file" "20160912.1859" "Open a junk (memo) file to try-and-error" 'nil :url "http://www.emacswiki.org/cgi-bin/wiki/download/open-junk-file.el" :keywords '("convenience" "tools"))
