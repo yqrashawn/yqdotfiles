a0b0061b7b93b82f9badf49d9f259aed54306787
yqrashawn <namy.19@gmail.com>
2016-12-25 22:41:39 +0800

    add imenu anywhere ivy-rich, remove e in normal state, fix init.vim
    Former-commit-id: 2dd38e607218fc763dff0de70b142eede68eb692

--- .emacs.d/elpa/zoom-window-20161123.405/zoom-window-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; zoom-window-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "zoom-window" "zoom-window.el" (22598 57846
-;;;;;;  0 0))
-;;; Generated autoloads from zoom-window.el
-
-(autoload 'zoom-window-setup "zoom-window" "\
-To work with elscreen or persp-mode.
-
-\(fn)" nil nil)
-
-(autoload 'zoom-window-zoom "zoom-window" "\
-
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; zoom-window-autoloads.el ends here
--- .emacs.d/elpa/zoom-window-20161123.405/zoom-window-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "zoom-window" "20161123.405" "Zoom window like tmux" '((emacs "24.3")) :url "https://github.com/syohex/emacs-zoom-window")
