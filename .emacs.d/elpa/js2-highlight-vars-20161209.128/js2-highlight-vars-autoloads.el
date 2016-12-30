acc98ba78944c2cf864a8a2bd3421f0f1b5885bc
yqrashawn <namy.19@gmail.com>
2016-12-30 14:04:33 +0800

    update alfred, delete js2 highlight var

--- .emacs.d/elpa/js2-highlight-vars-20161209.128/js2-highlight-vars-autoloads.el
+++
@@ -1,23 +0,0 @@
-;;; js2-highlight-vars-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "js2-highlight-vars" "js2-highlight-vars.el"
-;;;;;;  (22605 52770 0 0))
-;;; Generated autoloads from js2-highlight-vars.el
-
-(autoload 'js2-highlight-vars-mode "js2-highlight-vars" "\
-Minor mode that highlights occurrences of the variable under
-cursor in js2-mode buffers
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; js2-highlight-vars-autoloads.el ends here
