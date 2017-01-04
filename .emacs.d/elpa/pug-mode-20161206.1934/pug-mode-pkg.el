66a30ea5803212d6aa37bb2e439e2fa04f501b49
yqrashawn <namy.19@gmail.com>
2017-01-04 11:31:55 +0800

    update evil-cp
    Former-commit-id: 99141c5922279d07d47cb752c7e05a74a01995cb

---
+++ .emacs.d/elpa/pug-mode-20161206.1934/pug-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "pug-mode" "20161206.1934" "Major mode for jade/pug template files" '((cl-lib "0.5")) :url "https://github.com/hlissner/emacs-pug-mode" :keywords '("markup" "language" "jade" "pug"))
