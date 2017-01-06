5450799914f039eff39da689d2883030d1c9f34d
yqrashawn <namy.19@gmail.com>
2017-01-06 11:56:38 +0800

    update emacs pacakges

---
+++ .emacs.d/elpa/dactyl-mode-20140906.1025/dactyl-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dactyl-mode" "20140906.1025" "Major mode for editing Pentadactyl config files" 'nil :url "https://github.com/luxbock/dactyl-mode" :keywords '("languages" "vim"))
---
+++ .emacs.d/elpa/rcirc-notify-20150219.1404/rcirc-notify-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rcirc-notify" "20150219.1404" "libnotify popups" 'nil :keywords '("lisp" "rcirc" "irc" "notify" "growl"))
