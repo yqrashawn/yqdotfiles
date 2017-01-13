aeb6101276e22820fbaad6decaecd94754c681da
yqrashawn <namy.19@gmail.com>
2017-01-13 16:58:39 +0800

    update packages

--- .emacs.d/elpa/hydra-20161204.505/hydra-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "hydra" "20161204.505" "Make bindings that stick around."
-  '((cl-lib "0.5"))
-  :url "https://github.com/abo-abo/hydra" :keywords
-  '("bindings"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
