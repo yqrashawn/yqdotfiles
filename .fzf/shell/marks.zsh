a78eb647e883df0873af49edb7ea83a4b6f1d857
yqrashawn <namy.19@gmail.com>
2017-02-18 16:32:37 +0800

    add fzf
    Former-commit-id: 5455bd9164cf20b9c5ceeedaacdc2dc2f8e55451

---
+++ .fzf/shell/marks.zsh
@@ -0,0 +1,45 @@
+if [[ -z $BOOKMARKS_FILE ]] ; then
+    export BOOKMARKS_FILE="$HOME/.bookmarks"
+fi
+
+if [[ ! -f $BOOKMARKS_FILE ]]; then
+    touch $BOOKMARKS_FILE
+fi
+
+function mark() {
+    echo $1 : $(pwd) >> $BOOKMARKS_FILE
+}
+
+fzfcmd() {
+   [ ${FZF_TMUX:-1} -eq 1 ] && echo "fzf-tmux -d${FZF_TMUX_HEIGHT:-40%}" || echo "fzf"
+}
+
+jump() {
+    local jumpline jumpdir
+
+    jumpline=$(cat ${BOOKMARKS_FILE} | $(fzfcmd) --bind=ctrl-y:accept --tac)
+    if [[ -n ${jumpline} ]]; then
+        jumpdir=$(echo $jumpline | awk '{print $3}' | sed "s#~#$HOME#")
+        sed -i --follow-symlinks "\#${jumpline}#d" $BOOKMARKS_FILE
+        cd ${jumpdir} && echo ${jumpline} >> $BOOKMARKS_FILE
+    fi
+    zle && zle reset-prompt
+}
+
+function dmark()  {
+    local marks_to_delete line
+
+    marks_to_delete=$(cat $BOOKMARKS_FILE | $(fzfcmd) -m --bind=ctrl-y:accept,ctrl-t:toggle-up --tac)
+
+    if [[ -n ${marks_to_delete} ]]; then
+        while read -r line; do
+            sed -i --follow-symlinks "\#${line}#d" $BOOKMARKS_FILE
+        done <<< "$marks_to_delete"
+
+        echo "** The following marks were deleted **"
+        echo ${marks_to_delete}
+    fi
+}
+
+zle -N jump
+bindkey '^w' jump
