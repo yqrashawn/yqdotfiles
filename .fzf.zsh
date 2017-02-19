00297cb12ebcc60fc366cbbdbfba4bb8cf7eb50f
yqrashawn <namy.9@gmail.com>
2017-02-19 20:08:34 +0800

    fix fzf.zsh
    Former-commit-id: c8c0587c438395a3e6f5e30af4a813c9c99c7062

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,18 +1,18 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/Rashawn/.fzf/bin* ]]; then
-  export PATH="$PATH:/Users/Rashawn/.fzf/bin"
+if [[ ! "$PATH" == *${HOME}.fzf/bin* ]]; then
+  export PATH="$PATH:${HOME}.fzf/bin"
 fi
 
 # Man path
 # --------
-if [[ ! "$MANPATH" == */Users/Rashawn/.fzf/man* && -d "/Users/Rashawn/.fzf/man" ]]; then
-  export MANPATH="$MANPATH:/Users/Rashawn/.fzf/man"
+if [[ ! "$MANPATH" == *${HOME}.fzf/man* && -d "${HOME}.fzf/man" ]]; then
+  export MANPATH="$MANPATH:${HOME}.fzf/man"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/Users/Rashawn/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "${HOME}.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 bindkey '^E' jump
