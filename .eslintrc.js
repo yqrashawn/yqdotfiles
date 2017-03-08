bf226f3cb05b48f9bd49acbe195d901b360a0fb8
yqrashawn <namy.19@gmail.com>
2017-03-08 13:29:44 +0800

    update
    Former-commit-id: d131708933c42173c15328641d47108dacd8ec47

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,121 +24,46 @@ module.exports = {
     'babel/generator-star-spacing': 1,
     'linebreak-style': [0],
     'babel/arrow-parens': 1,
-    indent: [
-      'off',
-      2,
-    ],
-    'arrow-parens': [
-      'off',
-    ],
-    'func-names': [
-      'off',
-    ],
-    'no-plusplus': [
-      'off',
-    ],
+    'no-extend-native': ['off',],
+    indent: ['off', 2,],
+    'no-extend-native': ['off',],
+    'arrow-parens': ['off',],
+    'func-names': ['off',], 'no-plusplus': ['off',],
+    'import/prefer-default-export': ['off',],
+    'babel/arrow-parens': ['off',],
+    'new-cap': ['off',],
+    'import/no-unresolved': ['off',],
+    'no-useless-escape': ['off',],
+    'import/no-extraneous-dependencies': ['off',],
+    'global-require': ['off',],
+    'no-lonely-if': ['off',],
+    'no-iterator': ['error',],
+    'no-alert': ['off',],
+    'no-multi-str': ['off',],
+    'comma-dangle': ['off',],
+    'no-restricted-syntax': ['off', 'ForInStatement',],
+    'no-console': ['off',],
+    curly: ['off',],
+    'no-unused-expressions': ['off',],
+    'no-unused-vars': ['off',],
+    'spaced-comment': ['warn',],
+    'no-use-before-define': ['off',],
+    'no-param-reassign': ['off',],
+    'no-mixed-operators': ['off',],
+    'no-shadow': ['off',],
+    'no-restricted-syntax': ['warn',],
+    'no-else-return': ['warn',],
+    'no-underscore-dangle': ['off',],
+    quotes: ['off', 'single',],
+    semi: ['error', 'always',],
+    'no-prototype-builtins': ['off',],
+    'consistent-return': ['off',],
+    'max-len': ['error', 300],
     'valid-jsdoc': [
       'error',
       {
         requireReturn: false,
       },
-
-      // {
-      //   preferType: {
-      //     Boolean: 'boolean',
-      //     Number: 'number',
-      //     object: 'Object',
-      //     String: 'string',
-      //   },
-      // },
     ],
-    'babel/arrow-parens': [
-      'off',
-    ],
-    'new-cap': [
-      'off',
-    ],
-    'import/no-unresolved': [
-      'off',
-    ],
-    'no-useless-escape': [
-      'off',
-    ],
-    'import/no-extraneous-dependencies': [
-      'off',
-    ],
-    'global-require': [
-      'off',
-    ],
-    'no-lonely-if': [
-      'off',
-    ],
-    'no-iterator': [
-      'error',
-    ],
-    'no-alert': [
-      'off',
-    ],
-    'no-multi-str': [
-      'off',
-    ],
-    'comma-dangle': [
-      'off',
-    ],
-    'no-restricted-syntax': [
-      'off',
-      'ForInStatement',
-    ],
-    'no-console': [
-      'off',
-    ],
-    curly: [
-      'off',
-    ],
-    'no-unused-expressions': [
-      'off',
-    ],
-    'no-unused-vars': [
-      'off',
-    ],
-    'spaced-comment': [
-      'warn',
-    ],
-    'no-use-before-define': [
-      'off',
-    ],
-    'no-param-reassign': [
-      'off',
-    ],
-    'no-mixed-operators': [
-      'off',
-    ],
-    'no-shadow': [
-      'off',
-    ],
-    'no-restricted-syntax': [
-      'warn',
-    ],
-    'no-else-return': [
-      'warn',
-    ],
-    'no-underscore-dangle': [
-      'off',
-    ],
-    quotes: [
-      'off',
-      'single',
-    ],
-    semi: [
-      'error',
-      'always',
-    ],
-    'no-prototype-builtins': [
-      'off',
-    ],
-    'consistent-return': [
-      'off',
-    ],
-    'max-len': ['error', 300],
   },
 };
