5b3946a3c5775ae4b9ecc667b89bcdcb2c1da1de
yqrashawn <namy.19@gmail.com>
2017-03-30 10:11:16 +0800

    add untabify
    Former-commit-id: 7baa81ab873363d25cd45510b525e1ac4bef7198

---
+++ .my_emacs/untabify.el
@@ -0,0 +1,11 @@
+(defvar untabify-this-buffer)
+(defun untabify-all ()
+  "Untabify the current buffer, unless `untabify-this-buffer' is nil."
+  (and untabify-this-buffer (untabify (point-min) (point-max))))
+
+(define-minor-mode untabify-mode
+  "Untabify buffer on save." nil " untab" nil
+  (make-variable-buffer-local 'untabify-this-buffer)
+  (setq untabify-this-buffer (not (derived-mode-p 'makefile-mode)))
+  (add-hook 'before-save-hook #'untabify-all))
+(add-hook 'prog-mode-hook 'untabify-mode)
