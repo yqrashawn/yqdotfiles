e625f34df910c71df88d505c5d52ab1c8d8a343f
yqrashawn <namy.19@gmail.com>
2017-05-09 09:17:48 +0800

    update prodigy
    Former-commit-id: 72390a0ceabacb2c3c33dc28d9e27795b4214303

--- .my_emacs/prodigy.el
+++ .my_emacs/prodigy.el
@@ -1,74 +1,16 @@
 (prodigy-define-service
-  :name "lmv front"
+  :name "lmv front dev"
   :command "npm"
-  :cwd "~/workspace/gltflmvviewer/"
+  :cwd "~/workspace/OFFICE/gltflmvviewer/"
   :args '("run" "dev")
   :tags '(front lmv)
   )
 
 (prodigy-define-service
-  :name "peacock front"
-  :command "npm"
-  :cwd "~/workspace/peacock/client"
-  :args '("run" "dev")
-  :tags '(front peacock)
-  )
-
-(prodigy-define-service
-  :name "peacock back"
-  :command "npm"
-  :cwd "~/workspace/peacock/server"
-  :args '("run" "dev")
-  :tags '(back peacock)
-  )
-
-(prodigy-define-service
-  :name "sso back"
-  :command "npm"
-  :cwd "~/workspace/BimsopSSO/sso-server"
-  :args '("run" "dev")
-  :tags '(back nodemon)
-  )
-
-(prodigy-define-service
-  :name "sso front"
-  :command "npm"
-  :cwd "~/workspace/BimsopSSO/sso-client"
-  :args '("run" "dev")
-  :tags '(front webpack)
-  )
-
-(prodigy-define-service
-  :name "sso front"
-  :command "npm"
-  :cwd "~/workspace/BimsopSSO/sso-client"
-  :args '("run" "dev")
-  :tags '(front webpack)
-  )
-
-(prodigy-define-service
-  :name "gbim-diff server"
-  :command "npm"
-  :cwd "~/workspace/gbim-diff"
-  :args '("run" "dev")
-  :tags '(nodemon back)
-  )
-
-(prodigy-define-service
-  :name "mongodb sso"
-  :command "mongod"
-  :cwd "~/workspace/BimsopSSO/"
-  :args '("-dbpath" "./dbpath")
-  :tags '(mongod back)
-  )
-
-
-;; macbook
-(prodigy-define-service
-  :name "lmv front"
-  :command "npm"
+  :name "lmv front watch"
+  :command "webpack"
   :cwd "~/workspace/OFFICE/gltflmvviewer/"
-  :args '("run" "dev")
+  :args '("-w")
   :tags '(front lmv)
   )
 
