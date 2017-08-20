339c659e90e36ec9a0eabfd58a0dae216e10ebcc
yqrashawn <namy.19@gmail.com>
2017-08-20 14:56:47 +0800

    indium nodejs-repl
    Former-commit-id: 248cdbee18caa521901d9b0d84d26c8646da764b

--- .my_emacs/javascript.el
+++ .my_emacs/javascript.el
@@ -22,6 +22,35 @@
 ;;         ad-do-it)
 ;;     ad-do-it))
 
+(defun nodejs-repl-resend-buffer ()
+  (interactive)
+  (nodejs-repl-quit-or-cancel)
+  (nodejs-repl-send-buffer)
+  )
+
+(use-package nodejs-repl
+  :init
+  (progn
+    (spacemacs/declare-prefix-for-mode 'js2-mode "mn" "nodejs")
+    (spacemacs/set-leader-keys-for-major-mode 'js2-mode
+      "ne" 'nodejs-repl-send-last-expression
+      "nk" 'nodejs-repl-send-reglin
+      "nj" 'nodejs-repl-send-line
+      "nf" 'nodejs-repl-load-file
+      "n," 'nodejs-repl-send-buffer
+      "n." 'nodejs-repl-quit-or-cancel
+      "nl" 'nodejs-repl-switch-to-repl
+      "nn" 'nodejs-repl-resend-buffer
+      )
+    (add-hook 'js2-mode-hook
+              (lambda ()
+                (define-key js2-mode-map (kbd "C-x C-e") 'nodejs-repl-send-last-expression)
+                (define-key js2-mode-map (kbd "C-c C-j") 'nodejs-repl-send-line)
+                (define-key js2-mode-map (kbd "C-c C-r") 'nodejs-repl-send-region)
+                (define-key js2-mode-map (kbd "C-c C-l") 'nodejs-repl-load-file)
+                (define-key js2-mode-map (kbd "C-c C-z") 'nodejs-repl-switch-to-repl)))
+    ))
+
   ;;;;;;;;;;;;; flycheck ;;;;;;;;;;;;;
 (setq flycheck-javascript-eslint-executable "eslint_d")
 
