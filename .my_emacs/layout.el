78acdc9c09f63291c6e6e0051ef999b1d7ceefad
yqrashawn <namy.19@gmail.com>
2017-08-23 22:09:14 +0800

    layout
    Former-commit-id: 9859892c097ce14cf340c9d39eaa6f845a8a51d4

--- .my_emacs/layout.el
+++ .my_emacs/layout.el
@@ -1,12 +1,40 @@
-(spacemacs|define-custom-layout "@shell"
-  :binding "s"
+(define-key evil-normal-state-map "su" 'spacemacs/select-custom-layout)
+(spacemacs|define-custom-layout "@LMV"
+  :binding "l"
   :body
   (progn
-    ;; hook to add all ERC buffers to the layout
-    (defun spacemacs-layouts/add-erc-buffer-to-persp ()
+    (defconst lmv-spacemacs-layout-name      "LMV" "LMV")
+    (defun spacemacs-layouts/add-lmv-buffer-to-persp ()
+      (if (or
+           (string-match "gltflmvviewer" buffer-file-truename))
+          (persp-add-buffer (current-buffer)
+                            (persp-get-by-name
+                             lmv-spacemacs-layout-name))))
+    (add-hook 'prog-mode-hook #'spacemacs-layouts/add-lmv-buffer-to-persp)
+    (find-file "~/workspace/OFFICE/gltflmvviewer/")
+    (prodigy-start-service (prodigy-find-service "lmv front dev"))))
+
+(spacemacs|define-custom-layout "@MAIL"
+  :binding "m"
+  :body
+  (progn
+    (defconst mail-spacemacs-layout-name      "Mail" "Mail")
+    (defun spacemacs-layouts/add-mail-buffer-to-persp ()
       (persp-add-buffer (current-buffer)
                         (persp-get-by-name
-                         erc-spacemacs-layout-name)))
-    (add-hook 'erc-mode-hook #'spacemacs-layouts/add-erc-buffer-to-persp)
-    ;; Start multi-term
-    (call-interactively 'multi-term)))
+                         mail-spacemacs-layout-name)))
+    (mu4e)))
+
+(spacemacs|define-custom-layout "@ELISP"
+  :binding "e"
+  :body
+  (progn
+    (defconst mail-spacemacs-layout-name      "Elisp" "Elisp")
+    (defun spacemacs-layouts/add-elsip-buffer-to-persp ()
+      (if (or (string-match (find-buffer-visiting (current-buffer)))
+              (eq major-mode 'emacs-lisp-mode))
+          (persp-add-buffer (current-buffer)
+                            (persp-get-by-name
+                             lmv-spacemacs-layout-name))))
+    (add-hook 'emacs-lisp-mode #'spacemacs-layouts/add-lmv-buffer-to-persp)
+    (spacemacs/find-dotfile)))
