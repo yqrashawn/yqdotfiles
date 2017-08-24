ff7a0775e63621168e4a8828e8be96e3b0a9d729
yqrashawn <namy.19@gmail.com>
2017-08-24 14:20:44 +0800

    update
    Former-commit-id: f2698b9ae0c7caa277de73834e876bd34832870a

--- .emacs.d/private/layers/navigation/packages.el
+++ .emacs.d/private/layers/navigation/packages.el
@@ -2,19 +2,8 @@
 ;; Author: yqrashawn <namy.19@gmail.com>
 (setq navigation-packages
       '(
-        iflipb
         ace-jump-buffer
         ))
-
-(defun navigation/init-iflipb ()
-  (use-package iflipb
-    :init
-    (progn
-      (setq iflipb-wrap-around t)
-      (setq iflipb-permissive-flip-back t)
-      (global-set-key (kbd "M-l") 'iflipb-next-buffer)
-      (global-set-key (kbd "M-h") 'iflipb-previous-buffer)
-      )))
 
 (defun navigation/init-ace-jump-buffer ()
   (use-package ace-jump-buffer
