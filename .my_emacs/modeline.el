be3282132b9ab3cd1bbfa2a4b3acf0d82616d1d1
yqrashawn <namy.19@gmail.com>
2017-09-12 08:56:34 +0800

    up
    Former-commit-id: 4c412122a7a54215e349d63774987cf69d251cfd

--- .my_emacs/modeline.el
+++ .my_emacs/modeline.el
@@ -1,1 +1,17 @@
 (setq-default powerline-height 8)
+(setq mu4e-alert-mode-line t)
+(setq evil-mode-line-format '(before . mode-line-front-space))
+(add-to-list 'sml/replacer-regexp-list
+             '("^~/workspace/THIRD/three.js" ":THREE:"))
+(add-to-list 'sml/replacer-regexp-list
+             '("^~/workspace/HOME/" ":HOME:"))
+(add-to-list 'sml/replacer-regexp-list
+             '("^~/workspace/OFFICE/" ":WORK:"))
+(add-to-list 'sml/replacer-regexp-list
+             '("^~/workspace/OFFICE/gltflmvviewer" ":LMV:"))
+(add-to-list 'sml/replacer-regexp-list
+             '("^~/workspace/OFFICE/gltflmvviewer/src" ":LMVS:"))
+(add-to-list 'sml/replacer-regexp-list
+             '("^~/workspace/OFFICE/gltflmvviewer/src/extensions" ":LMV|E:"))
+(add-to-list 'sml/replacer-regexp-list
+             '("^~/\\.emacs\\.d/" ":CONF:"))
