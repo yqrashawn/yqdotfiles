5574b03ac751f0dc96a409068b7e63bf3e7e2178
yqrashawn <namy.19@gmail.com>
2017-10-03 10:44:17 +0800

    update
    Former-commit-id: 5f5bc0d88b6733c0dc3bce14cb73c4260e8a327e

--- .my_emacs/javascript.el
+++ .my_emacs/javascript.el
@@ -125,3 +125,16 @@
             ;; Restore point and scroll position
             (goto-char current-point)
             (recenter (- line 1))))))))
+
+;; Enable JavaScript completion between <script>...</script> etc.
+(defadvice company-tern (before web-mode-set-up-ac-sources activate)
+  "Set `tern-mode' based on current language before running company-tern."
+  (message "advice")
+  (if (equal major-mode 'web-mode)
+      (let ((web-mode-cur-language
+             (web-mode-language-at-pos)))
+        (if (or (string= web-mode-cur-language "javascript")
+                (string= web-mode-cur-language "jsx")
+                )
+            (unless tern-mode (tern-mode))
+          (if tern-mode (tern-mode -1))))))
