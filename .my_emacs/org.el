f0ca14f0e17e4516ce9c279930356b384e2ea966
yqrashawn <namy.19@gmail.com>
2017-10-14 14:55:32 +0800

    config layer in right place
    Former-commit-id: 374b231ae03ae44b563e3d0f8858df11447dd003

--- .my_emacs/org.el
+++ .my_emacs/org.el
@@ -4,8 +4,6 @@
 (setq org-mobile-inbox-for-pull "~/Dropbox/org/flagged.org")
 (setq org-agenda-files '("~/Dropbox/org"))
 (setq org-default-notes-file '("~/Dropbox/org/notes.org"))
-(setq-default dotspacemacs-configuration-layers
-              '((org :variables org-projectile-file "plans.org")))
 (with-eval-after-load 'org-agenda
   (require 'org-projectile)
   (setq org-agenda-files (append org-agenda-files (org-projectile:todo-files))))
