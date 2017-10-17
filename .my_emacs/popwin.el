78b3d298051a46c5b5b7bb3ba32ad5bf185037b7
yqrashawn <namy.19@gmail.com>
2017-10-17 09:42:50 +0800

    add circe, add el.el
    Former-commit-id: b92743d43995377d6ead78e829a431c525adede9

--- .my_emacs/popwin.el
+++ .my_emacs/popwin.el
@@ -4,3 +4,14 @@
 (push '("*quickrun*" :dedicated t :position bottom :stick t :noselect t) popwin:special-display-config)
 (push '("*warnings*" :dedicated t :position bottom :stick nil :noselect t) popwin:special-display-config)
 (push '("*git-messenger*" :dedicated t :position bottom :stick nil :noselect nil) popwin:special-display-config)
+
+;; purpose setting
+(add-to-list 'purpose-user-mode-purposes '(prog-mode . prog))
+(add-to-list 'purpose-user-mode-purposes '(circe-mode . irc))
+(add-to-list 'purpose-user-mode-purposes '(circe-chat-mode . irc))
+(add-to-list 'purpose-user-mode-purposes '(circe-query-mode . irc))
+(add-to-list 'purpose-user-mode-purposes '(circe-lagmon-mode . irc))
+(add-to-list 'purpose-user-mode-purposes '(circe-server-mode . irc))
+(purpose-compile-user-configuration)
+
+
