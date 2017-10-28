69a883434633a4d8747c987c6e0179ec7740b3d6
yqrashawn <namy.19@gmail.com>
2017-10-28 10:59:33 +0800

    fix fzf
    Former-commit-id: 11236a7c945cec4ea59f978b49ac1db1c79b51d9

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,14 +1,14 @@
 # Setup fzf
 # ---------
-# if [[ ! "$PATH" == */usr/local/Cellar/fzf/0.17.1/bin* ]]; then
-#   export PATH="$PATH:/usr/local/Cellar/fzf/0.17.1/bin"
+# if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+#   export PATH="$PATH:/usr/local/opt/fzf/bin"
 # fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/Cellar/fzf/0.17.1/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/Cellar/fzf/0.17.1/shell/key-bindings.zsh"
+source "/usr/local/opt/fzf/shell/key-bindings.zsh"
 
