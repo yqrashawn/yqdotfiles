71de4fa3589f9ef54bc13286b00f70628b7cac41
yqrashawn <namy.19@gmail.com>
2017-10-30 09:02:06 +0800

    no paste transient state
    Former-commit-id: f5074a80029874cde8b6558124154624b896f863

--- .my_emacs/aliases.el
+++ .my_emacs/aliases.el
@@ -8,7 +8,7 @@
 (defalias 'yasi 'yas-insert-snippet "insert snippets")
 (defalias 'yasl 'yas-reload-all "reload yas")
 (defalias 'reload 'reload-spacemacs-configuration "reload config")
-(defalias 'cd 'dired-to-home "dired home directory")
+(defalias 'cdd 'dired-to-home "dired home directory")
 
 (defun dired-to-home ()
   "open dired in home directory"
@@ -20,19 +20,19 @@
   (interactive)
   (load-file (concat (file-name-directory spacemacs-core-directory)
                      "core-load-paths.el"))
-  ;; (require 'core-spacemacs)
-  ;; (spacemacs/init)
+  (require 'core-spacemacs)
+  (spacemacs/init)
   (configuration-layer/load)
-  ;; (spacemacs/setup-startup-hook)
+  (spacemacs/setup-startup-hook)
   (require 'server)
   (unless (server-running-p) (server-start)))
 
 (use-package hackernews :commands (hackernews))
 
-(defun artist-mode-toggle-emacs-state ()
-  (if artist-mode
-      (evil-emacs-state)
-    (evil-exit-emacs-state)))
+;; (defun artist-mode-toggle-emacs-state ()
+;;   (if artist-mode
+;;       (evil-emacs-state)
+;;     (evil-exit-emacs-state)))
 
-(unless (eq dotspacemacs-editing-style 'emacs)
-  (add-hook 'artist-mode-hook #'artist-mode-toggle-emacs-state))
+;; (unless (eq dotspacemacs-editing-style 'emacs)
+;;   (add-hook 'artist-mode-hook #'artist-mode-toggle-emacs-state))
