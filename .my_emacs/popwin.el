e7827a955d0ec06786cc3048b4f17fbf8c8b77e9
yqrashawn <namy.19@gmail.com>
2017-11-07 09:29:32 +0800

    update popwin
    Former-commit-id: 1fed83418c3aa6ae4612010563e2ed76444d2cc0

--- .my_emacs/popwin.el
+++ .my_emacs/popwin.el
@@ -1,3 +1,4 @@
+(pupo-mode)
 (push '("*Messages*" :dedicated t :position bottom :stick nil :noselect nil) popwin:special-display-config)
 (push '("*ycmd-server*" :dedicated t :position bottom :stick nil :noselect t) popwin:special-display-config)
 (push '("*Flycheck errors*" :dedicated t :position bottom :stick nil :noselect t) popwin:special-display-config)
@@ -6,6 +7,7 @@
 (push '("*quickrun*" :dedicated t :position bottom :stick t :noselect t) popwin:special-display-config)
 (push '("*warnings*" :dedicated t :position bottom :stick nil :noselect t) popwin:special-display-config)
 (push '("*git-messenger*" :dedicated t :position bottom :stick nil :noselect nil) popwin:special-display-config)
+;; (push '("\\*helpful.+\\*" :dedicated t :position right :stick nil :noselect nil) popwin:special-display-config)
 
 ;; purpose setting
 ;; (add-to-list 'purpose-user-mode-purposes '(prog-mode . prog))
@@ -17,4 +19,5 @@
 (add-to-list 'purpose-user-mode-purposes '(circe-query-mode . irc))
 (add-to-list 'purpose-user-mode-purposes '(circe-lagmon-mode . irc))
 (add-to-list 'purpose-user-mode-purposes '(circe-server-mode . irc))
+(pupo/update-purpose-config)
 (purpose-compile-user-configuration)
