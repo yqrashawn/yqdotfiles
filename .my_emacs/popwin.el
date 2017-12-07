d93a087131f19c1cd4f126e942a1e15648e58bc1
yqrashawn <namy.19@gmail.com>
2017-12-07 21:01:24 +0800

    emacs, disable evil-escape-sequence, add pdf keybindings, add js2 indium prefix name
    Former-commit-id: 23ac0ce1c6f87e35c6cd66359ea0ae4910899053

--- .my_emacs/popwin.el
+++ .my_emacs/popwin.el
@@ -7,10 +7,11 @@
 (push '("*quickrun*" :dedicated t :position bottom :stick t :noselect t) popwin:special-display-config)
 (push '("*warnings*" :dedicated t :position bottom :stick nil :noselect t) popwin:special-display-config)
 (push '("*git-messenger*" :dedicated t :position bottom :stick nil :noselect nil) popwin:special-display-config)
-(push '("*Cargo Run*" :dedicated t :position right :stick nil :noselect nil) popwin:special-display-config)
-(push '("*Cargo Build*" :dedicated t :position right :stick nil :noselect nil) popwin:special-display-config)
+(push '("*Cargo Run*" :dedicated t :position bottom :stick nil :noselect nil) popwin:special-display-config)
+(push '("*Cargo Build*" :dedicated t :position bottom :stick nil :noselect nil) popwin:special-display-config)
 (push '("*Backtrace*" :dedicated t :position bottom :stick t :noselect nil) popwin:special-display-config)
 (push '("\\*helpful.+\\*" :dedicated t :position right :stick nil :noselect nil) popwin:special-display-config)
+(push '("*Outline.*\.pdf*" :dedicated t :position left :stick t :noselect nil) popwin:special-display-config)
 
 ;; purpose setting
 ;; (add-to-list 'purpose-user-mode-purposes '(prog-mode . prog))
