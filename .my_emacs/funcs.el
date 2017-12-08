c98aeaa38f211a0487940968ec4d675c68273a48
yqrashawn <namy.19@gmail.com>
2017-12-08 17:56:49 +0800

    fix karabiner conflict between alfred mode and tmux prefix mode, add pdf view midnight theme
    Former-commit-id: bff0dcb04b45bf1915a73c7d09cf23f983dc11cb

--- .my_emacs/funcs.el
+++ .my_emacs/funcs.el
@@ -837,3 +837,42 @@ string).  It returns t if a new completion is found, nil otherwise."
   :defer
   :commands nav-mode
   :mode ("\\.epub\\'" . nov-mode))
+
+;; pdf view mode midnite theme
+(setq pdf-view-midnight-colors '("#ff9900" . "#0a0a12" )) ; set the amber profile as default (see below)
+
+(defun bms/pdf-no-filter ()
+  "View pdf without colour filter."
+  (interactive)
+  (pdf-view-midnight-minor-mode -1)
+  )
+
+;; change midnite mode colours functions
+(defun bms/pdf-midnite-original ()
+  "Set pdf-view-midnight-colors to original colours."
+  (interactive)
+  (setq pdf-view-midnight-colors '("#839496" . "#002b36" )) ; original values
+  (pdf-view-midnight-minor-mode)
+  )
+
+(defun bms/pdf-midnite-amber ()
+  "Set pdf-view-midnight-colors to amber on dark slate blue."
+  (interactive)
+  (setq pdf-view-midnight-colors '("#ff9900" . "#0a0a12" )) ; amber
+  (pdf-view-midnight-minor-mode)
+  )
+
+(defun bms/pdf-midnite-green ()
+  "Set pdf-view-midnight-colors to green on black."
+  (interactive)
+  (setq pdf-view-midnight-colors '("#00B800" . "#000000")) ; green
+  (pdf-view-midnight-minor-mode))
+
+(defun bms/pdf-midnite-colour-schemes ()
+  "Midnight mode colour schemes bound to keys"
+        (local-set-key (kbd "!") (quote bms/pdf-no-filter))
+        (local-set-key (kbd "@") (quote bms/pdf-midnite-amber))
+        (local-set-key (kbd "#") (quote bms/pdf-midnite-green)
+            (local-set-key (kbd "$") (quote bms/pdf-midnite-original))))
+
+(setq counsel-find-file-occur-cmd "ls | grep -i -E '%s' | gxargs -d '\n' ls")
