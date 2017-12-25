610d8a64b553fdd213be1a2cc4b18ef6b402d7f6
yqrashawn <namy.19@gmail.com>
2017-12-25 18:27:12 +0800

    fix js2 hook, add warning buffer popwin
    Former-commit-id: 1ad43f8285ded645e6116f33d5064efed61c3b23

--- .my_emacs/popwin.el
+++ .my_emacs/popwin.el
@@ -6,6 +6,7 @@
 (push '("*Compile-Log*" :dedicated t :position bottom :stick nil :noselect t) popwin:special-display-config)
 (push '("*quickrun*" :dedicated t :position bottom :stick t :noselect t) popwin:special-display-config)
 (push '("*warnings*" :dedicated t :position bottom :stick nil :noselect t) popwin:special-display-config)
+(push '("*Warnings*" :dedicated t :position bottom :stick nil :noselect t) popwin:special-display-config)
 (push '("*git-messenger*" :dedicated t :position bottom :stick nil :noselect nil) popwin:special-display-config)
 (push '("*Cargo Run*" :dedicated t :position bottom :stick nil :noselect nil) popwin:special-display-config)
 (push '("*Cargo Build*" :dedicated t :position bottom :stick nil :noselect nil) popwin:special-display-config)
