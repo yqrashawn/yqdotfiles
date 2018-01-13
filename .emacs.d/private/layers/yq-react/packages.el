a94350f5fbd1f2f154a9e08aede6054ff55f8490
yqrashawn <namy.19@gmail.com>
2018-01-13 21:26:46 +0800

    add yq-react layer
    Former-commit-id: 4f71b507576240d99a734fb6523c0fea04477237

---
+++ .emacs.d/private/layers/yq-react/packages.el
@@ -0,0 +1,21 @@
+(setq yq-react-packages '(rjsx-mode
+                          prettier-js))
+
+
+(defun yq-react/init-rjsx-mode ()
+  (use-package rjsx-mode
+    :defer t
+    :init (progn
+            (add-to-list 'auto-mode-alist '("components\/.*\.js\'" . rjsx-mode))
+            (add-to-list 'auto-mode-alist '("containers\/.*\.js\'" . rjsx-mode)))
+    :mode (("\\.jsx\\'" . rjsx-mode))
+    :config (progn (evil-define-key 'insert rjsx-mode-map (kbd "C-d") 'rjsx-delete-creates-full-tag
+                     (spacemacs/set-leader-keys-for-major-mode 'rjsx-mode "=" 'prettier-js)
+                     (spacemacs/set-leader-keys-for-major-mode 'rjsx-mode "m" 'js2-mode)))
+    :commands (rjsx-mode)))
+
+
+(defun yq-react/init-prettier-js ()
+  (use-package prettier-js
+    :commands (prettier-js-mode prettier-js)
+    :defer t))
