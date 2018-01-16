1f8ade1a280b724df80731ed67dc14d913a28894
yqrashawn <namy.19@gmail.com>
2018-01-16 09:37:16 +0800

    add js config
    Former-commit-id: 3a86fe30643a6a86b945cf8f6b8c3202a0a08af9

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  parser: "babylon", //"babylon", "flow", "typescript", "postcss", "json", "graphql"
+  printWidth: 100,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "es5",
+};
