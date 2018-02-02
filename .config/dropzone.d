0fa91e3cbb8c66fb2a4cdc2bee00fc3a5f202ca2
yqrashawn <namy.19@gmail.com>
2018-02-02 17:07:30 +0800

    update dgbim dropzone.d
    Former-commit-id: fac21938d8377fd365021f3f648e80ccc17c6361

--- .config/dropzone.d
+++ .config/dropzone.d
@@ -2,5 +2,9 @@
   "Lmv_sampleModels_gbim": {
     "path": "~/workspace/OFFICE/gltflmvviewer/sampleModels/gbim/",
     "relative_level": 2
+  },
+  "Bimv_sampleModels_gbim": {
+    "path": "~/workspace/OFFICE/bimsop-viewer-bimv/sampleModels/gbim/",
+    "relative_level": 2
   }
 }
