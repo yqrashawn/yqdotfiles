58e4f62adb9f46a271880e670fd1a55c7d57a364
yqrashawn <namy.19@gmail.com>
2018-02-27 11:55:18 +0800

    add ui to youdao dic, add fd alias
    Former-commit-id: b3380177946d4760d2c3a53aded1cafe55ebe668

--- .config/.zsh.d/.alias.zsh
+++ .config/.zsh.d/.alias.zsh
@@ -81,3 +81,6 @@ command -v md5sum > /dev/null || alias md5sum="md5"
 
 # macOS has no `sha1sum`, so use `shasum` as a fallback
 command -v sha1sum > /dev/null || alias sha1sum="shasum"
+
+alias fdf='fd -t f'
+alias fdd='fd -t d'
