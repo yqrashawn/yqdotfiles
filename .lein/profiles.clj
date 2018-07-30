7a843d7b68cd112e443dc45f97bae8c582742ac8
yqrashawn <namy.19@gmail.com>
2018-07-30 08:06:29 +0800

    disable lein-midje
    Former-commit-id: c6a27fd7446cbd6b09200ebdc73d8ab13feb5cbf

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -5,6 +5,7 @@
                   [lein-cloverage "1.0.11"]
                   [lein-environ "1.1.0"]
                   [lein-bin "0.3.6"]
+                  ;; [lein-midje "3.2.1"]
                   [datawalk "0.1.4-SNAPSHOT"]
                   [lein-auto "0.1.3"]
                   [atroche/lein-ns-dep-graph "0.2.0-SNAPSHOT"]
