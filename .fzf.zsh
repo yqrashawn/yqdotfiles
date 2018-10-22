bcb591b0063b5f580c8b8b8f11b3c15c01faf439
yqrashawn <namy.19@gmail.com>
2018-10-22 17:17:10 +0800

    up[date fzf

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,18 +1,14 @@
 # Setup fzf
 # ---------
-# if [[ ! "$PATH" == */usr/local/Cellar/fzf/0.17.1/bin* ]]; then
-#   export PATH="$PATH:/usr/local/Cellar/fzf/0.17.1/bin"
-# fi
+if [[ ! "$PATH" == */usr/local/Cellar/fzf/0.17.5/bin* ]]; then
+  export PATH="$PATH:/usr/local/Cellar/fzf/0.17.5/bin"
+fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/Cellar/fzf/0.17.1/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/usr/local/Cellar/fzf/0.17.5/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "$HOME/.fzf/shell/key-bindings.zsh"
-
-TRAPWINCH() {
-    zle && { zle reset-prompt; zle -R }
-}
+source "/usr/local/Cellar/fzf/0.17.5/shell/key-bindings.zsh"
 
