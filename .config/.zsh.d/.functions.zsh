1d1df6f201e6afdffd1834a4c6c189948c9acd9f
yqrashawn <namy.19@gmail.com>
2019-04-17 11:59:50 +0800

    fix fzf version number, add cross platform link method

--- .config/.zsh.d/.functions.zsh
+++ .config/.zsh.d/.functions.zsh
@@ -124,3 +124,58 @@ function is-mobile {
 function cheat() {
     curl cht.sh/$1
 }
+
+# Pulled from https://stackoverflow.com/questions/18641864/git-bash-shell-fails-to-create-symbolic-links
+# https://stackoverflow.com/users/124119/camilo-martin
+
+# Detect windows (assumes we are in 'msysgit' or similar).
+windows() { [[ -n "$WINDIR" ]]; }
+
+# Cross-platform symlink function.
+#  With one parameter, it will check whether the parameter is a symlink.
+#  With two parameters, it will create a symlink to a file or directory,
+#   with syntax: link $linkname $target
+link() {
+    if [[ -z "$2" ]]; then
+        # Link-checking mode.
+        if windows; then
+            fsutil reparsepoint query "$1" > /dev/null
+        else
+            [[ -h "$1" ]]
+        fi
+    else
+        # Link-creation mode.
+        if windows; then
+            # Windows needs to be told if it's a directory or not. Infer that.
+            # Also: note that we convert `/` to `\`. In this case it's necessary.
+            if [[ -d "$2" ]]; then
+                cmd <<< "mklink /D \"$1\" \"${2//\//\\}\"" > /dev/null
+            else
+                cmd <<< "mklink \"$1\" \"${2//\//\\}\"" > /dev/null
+            fi
+        else
+            # You know what? I think ln's parameters are backwards.
+            ln -s "$2" "$1"
+        fi
+    fi
+}
+
+# Pulled from https://stackoverflow.com/questions/18641864/git-bash-shell-fails-to-create-symbolic-links
+# https://stackoverflow.com/users/124119/camilo-martin
+
+# Detect windows (assumes we are in 'msysgit' or similar).
+windows() { [[ -n "$WINDIR" ]]; }
+
+# Remove a link, cross-platform.
+rmlink() {
+  if windows; then
+    # Again, Windows needs to be told if it's a file or directory.
+    if [[ -d "$1" ]]; then
+      rmdir "$1";
+    else
+      rm "$1"
+    fi
+  else
+    rm "$1"
+  fi
+}
