2d92059bc6e312af2ec6cea3889b42795ccc912a
yqrashawn <namy.19@gmail.com>
2019-04-17 11:59:50 +0800

    fix fzf version number, add cross platform link method

--- .fzf.zsh
+++ .fzf.zsh
@@ -6,9 +6,9 @@ fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/Cellar/fzf/0.17.5/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/Cellar/fzf/0.17.5/shell/key-bindings.zsh"
+source "/usr/local/opt/fzf/shell/key-bindings.zsh"
 
