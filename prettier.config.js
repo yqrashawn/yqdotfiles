ca2f82b524df98e7c6fe9b9f8b56e1d4aa325e63
yqrashawn <namy.19@gmail.com>
2019-06-12 11:57:07 +0800

    Clean: remove global prettier config

--- prettier.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  parser: "babylon", //"babylon", "flow", "typescript", "postcss", "json", "graphql"
-  printWidth: 80,
-  semi: true,
-  singleQuote: false,
-  trailingComma: "es5",
-};
