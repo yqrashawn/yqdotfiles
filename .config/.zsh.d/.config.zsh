648fe4d84bdc5e3c79e7b896ee7372e50752882f
yqrashawn <namy.19@gmail.com>
2019-07-30 15:21:23 +0800

    disable powerline-shell

--- .config/.zsh.d/.config.zsh
+++ .config/.zsh.d/.config.zsh
@@ -47,22 +47,22 @@ fi
 # source "${ZDOTDIR:-$HOME}/.zprezto/modules/gitsome/init.sh"
 
 # https://github.com/b-ryan/powerline-shell
-function powerline_precmd() {
-    PS1="$(powerline-shell --shell zsh $?)"
-}
+# function powerline_precmd() {
+#     PS1="$(powerline-shell --shell zsh $?)"
+# }
 
-function install_powerline_precmd() {
-    for s in "${precmd_functions[@]}"; do
-        if [ "$s" = "powerline_precmd" ]; then
-            return
-        fi
-    done
-    precmd_functions+=(powerline_precmd)
-}
+# function install_powerline_precmd() {
+#     for s in "${precmd_functions[@]}"; do
+#         if [ "$s" = "powerline_precmd" ]; then
+#             return
+#         fi
+#     done
+#     precmd_functions+=(powerline_precmd)
+# }
 
-if [ "$TERM" != "linux" ]; then
-    install_powerline_precmd
-fi
+# if [ "$TERM" != "linux" ]; then
+#     install_powerline_precmd
+# fi
 
 : ${TERM_TITLE_SET_MULTIPLEXER:=1}
 
