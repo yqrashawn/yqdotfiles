98d5eefe2fdfe7c5dc394e888179000a502b2e4e
yqrashawn <namy.19@gmail.com>
2019-08-30 12:04:43 +0800

    Add: use alfred 4, use em

--- .config/.zsh.d/.alias.zsh
+++ .config/.zsh.d/.alias.zsh
@@ -49,7 +49,7 @@ alias serv="live-server --port=8081"
 alias httpg="http-server -g"
 
 # editors
-alias e="emacsclient -n"
+# alias e="emacsclient -n"
 alias ee='fasd -f -e emacsclient -nw'
 alias eee='emacsclient -nw'
 alias demacs='emacs -Q -l ~/.emacs.d/init.debug.el'
