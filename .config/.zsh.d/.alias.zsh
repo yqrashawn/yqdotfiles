a850a956ac571719abee6e89a5dd8fd1777181a4
yqrashawn <namy.19@gmail.com>
2020-02-14 09:39:38 +0800

    Add: gui and cli emacs

--- .config/.zsh.d/.alias.zsh
+++ .config/.zsh.d/.alias.zsh
@@ -51,7 +51,7 @@ alias httpg="http-server -g"
 
 # editors
 # alias e="emacsclient -n"
-alias ee='emacsclient -nw'
+# alias ee='emacsclient -nw'
 alias eee='emacsclient -nw'
 alias demacs='emacs -Q -l ~/.emacs.d/init.debug.el'
 alias v='nvim'
