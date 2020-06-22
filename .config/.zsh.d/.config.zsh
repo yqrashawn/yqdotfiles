42501df49309776c0a922fcec1eb1061d709e423
yqrashawn <namy.19@gmail.com>
2020-06-22 13:50:11 +0800

    chore: cleanup config

--- .config/.zsh.d/.config.zsh
+++ .config/.zsh.d/.config.zsh
@@ -1,3 +1,41 @@
+## vterm
+# track prompt in vterm
+vterm_prompt_end() {
+  vterm_printf "51;A$(whoami)@$(hostname):$(pwd)";
+}
+setopt PROMPT_SUBST
+PROMPT=$PROMPT'%{$(vterm_prompt_end)%}'
+
+if [[ "$INSIDE_EMACS" = 'vterm' ]]; then
+  alias clear='vterm_printf "51;Evterm-clear-scrollback";tput clear'
+fi
+
+function vterm_printf() {
+  if [ -n "$TMUX" ]; then
+    # Tell tmux to pass the escape sequences through
+    # (Source: http://permalink.gmane.org/gmane.comp.terminal-emulators.tmux.user/1324)
+    printf "\ePtmux;\e\e]%s\007\e\\" "$1"
+  elif [ "${TERM%%-*}" = "screen" ]; then
+    # GNU screen (screen, screen-256color, screen-256color-bce)
+    printf "\eP\e]%s\007\e\\" "$1"
+  else
+    printf "\e]%s\e\\" "$1"
+  fi
+}
+
+autoload -U add-zsh-hook
+add-zsh-hook -Uz chpwd (){ print -Pn "\e]2;%m:%2~\a" }
+
+function vterm_cmd() {
+  local vterm_elisp
+  vterm_elisp=""
+  while [ $# -gt 0 ]; do
+    vterm_elisp="$vterm_elisp""$(printf '"%s" ' "$(printf "%s" "$1" | sed -e 's|\\|\\\\|g' -e 's|"|\\"|g')")"
+    shift
+  done
+  vterm_printf "51;E$vterm_elisp"
+}
+
 if [ -n "$INSIDE_EMACS" ]; then
     chpwd() { print -P "\033AnSiTc %d" }
     print -P "\033AnSiTu %n"
@@ -136,3 +174,9 @@ fi
 # autoload -Uz add-zsh-hook
 # add-zsh-hook precmd term_title_precmd
 # add-zsh-hook preexec term_title_preexec
+
+# gitignore
+function gi() { curl -sLw n https://www.gitignore.io/api/$@ ;}
+
+test -e "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc" && source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'
+test -e '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc' && source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
