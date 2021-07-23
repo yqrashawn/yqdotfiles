fbc61cd9fb43682a193056b6bbda72255b5596fd
yqrashawn <namy.19@gmail.com>
2021-07-23 10:15:14 +0800

    feat: add modus theme to kitty and alacritty, try alacritty again

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -33,7 +33,7 @@
        ;;(emoji +unicode)  ; 🙂
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        hydra
-       ;;indent-guides     ; highlighted indent columns
+       indent-guides     ; highlighted indent columns
        ;;(ligatures +extra +pragmata-pro)         ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
