d2432ef31812b31b401b8993e6e54de1d91cb250
yqrashawn <namy.19@gmail.com>
2021-09-14 11:31:00 +0800

    chore: minor

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -33,7 +33,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory (expand-file-name "~/Dropbox/ORG"))
+(setq org-directory (expand-file-name "~/Dropbox/ORG/"))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
