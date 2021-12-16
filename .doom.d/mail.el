0a731090e8220e02cfcb52ae2870a02c79f869e5
yqrashawn <namy.19@gmail.com>
2021-12-16 19:20:19 +0800

    chore: notmuch

--- .doom.d/mail.el
+++ .doom.d/mail.el
@@ -9,9 +9,14 @@
 ;; (setq mu4e-html2text-command "html2text -width 72")
 
 (after! notmuch
+  (setq +notmuch-home-function (cmd!
+                                (notmuch-search "tag:inbox")
+                                (+notmuch/update)))
   (defun +notmuch-get-sync-command ()
-  "custom notmuch sync command"
-  "cd ~/.mail/account.gmail && gmi sync && cd ~/.mail/account.yqrashawn && gmi sync && notmuch new")
+    "custom notmuch sync command"
+    "cd ~/.mail/account.gmail && gmi sync && cd ~/.mail/account.yqrashawn && gmi sync && notmuch new")
   (setq! mm-text-html-renderer 'w3m-standalone)
   (setq! +notmuch-delete-tags '("+deleted" "-inbox" "-unread")
-         notmuch-show-indent-messages-width 2))
+         notmuch-show-indent-messages-width 2)
+  (set-popup-rule! "^\\*notmuch-hello" :ignore t)
+  (set-popup-rule! "^\\*subject:" :ignore t))
