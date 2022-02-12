2a872e33c1495c94ef1d36ce38d8d477909f8250
yqrashawn <namy.19@gmail.com>
2022-02-12 22:05:40 +0800

    fix: sketchybar notmuch

--- .config/sketchybar/plugins/notmuch.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cd $HOME/.mail/account.gmail && $HOME/.pyenv/shims/gmi sync && cd $HOME/.mail/account.yqrashawn && $HOME/.pyenv/shims/gmi sync
-notmuch new
-COUNT=$(notmuch count tag:inbox)
-sketchybar -m --set $NAME label="$COUNT"
