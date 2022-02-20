cf0ba3dbc7f1f21707d9c150c97e7e5cf095f8b8
yqrashawn <namy.19@gmail.com>
2022-02-20 18:26:12 +0800

    chore: misc

--- .doom.d/clojure.el
+++ .doom.d/clojure.el
@@ -67,6 +67,7 @@
         (apply func args))))
 
 (after! cider
+  (setq! cider-default-cljs-repl 'shadow)
   (setq! cider-auto-jump-to-error nil)
   ;; When asking for a "matching" REPL (clj/cljs), and no matching REPL is found,
   ;; return any REPL that is there. This is so that cider-quit can be called
