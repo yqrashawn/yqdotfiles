411a1d4b117ef6748f3e336eedebb960ac1ff181
yqrashawn <namy.19@gmail.com>
2022-04-08 12:15:02 +0800

    fix: sketchybar

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -139,7 +139,7 @@ See the docstrings of `defalias' and `make-obsolete' for more details."
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        hydra
        indent-guides     ; highlighted indent columns
-       (ligatures +extra +pragmata-pro) ; ligatures and symbols to make your code pretty again
+       ;; (ligatures +extra +pragmata-pro) ligatures and symbols to make your code pretty again
        minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
        nav-flash         ; blink cursor line after big motions
