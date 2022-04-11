2e11de4352a198cb8adb2935cfac562cefbd2b8f
yqrashawn <namy.19@gmail.com>
2022-04-11 18:29:34 +0800

    chore: cleanup

--- .doom.d/.dir-locals.el
+++
@@ -1,5 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((nil . ((lsp-enable-file-watchers . nil)
-         (projectile-project-root . "~/.doom.d/"))))
