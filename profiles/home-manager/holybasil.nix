d421b66221815e66b678d73be67d8308f32f19f9
yqrashawn <namy.19@gmail.com>
2022-06-07 14:25:44 +0800

    fix: git gpg sign

--- profiles/home-manager/holybasil.nix
+++ profiles/home-manager/holybasil.nix
@@ -2,9 +2,5 @@
   programs.git = {
     userEmail = "holybasil.1128@gmail.com";
     userName = "holybasil";
-    signing = {
-      key = "holybasil.1128@gmail.com";
-      signByDefault = true;
-    };
   };
 }
