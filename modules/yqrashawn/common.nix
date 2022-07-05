aaf8d7ece1351bb88d61b30adddce8ecfc172dc8
yqrashawn <namy.19@gmail.com>
2022-07-05 10:16:00 +0800

    fix: python3.10 pkgs

--- modules/yqrashawn/common.nix
+++ modules/yqrashawn/common.nix
@@ -1,4 +1,7 @@
-{ inputs, config, pkgs, ... }: {
+{ inputs, config, pkgs, ... }:
+let
+  stables = with pkgs.stable; [ notmuch yt-dlp streamlink mpv you-get python3 ];
+in {
   imports = [ ./primary.nix ./nixpkgs.nix ./overlays.nix ./etc-zsh.nix ];
 
   user = {
@@ -22,234 +25,228 @@
 
   # environment setup
   environment = {
-    systemPackages = with pkgs; [
-      # emacsMacport
-      # clojure
-      clojure-lsp
-      ispell
-      isync
-      babashka
-      joker
-      leiningen
-      obb
-      # nbb
+    systemPackages = with pkgs;
+      [
+        # emacsMacport
+        # clojure
+        clojure-lsp
+        ispell
+        isync
+        babashka
+        joker
+        leiningen
+        obb
+        # nbb
 
-      # tools
-      notmuch
-      vim
-      cachix
-      tmux
-      zsh
-      bash
-      git
-      neovim
-      mu
-      # macvim # use vim
-      bitwarden-cli
-      # firefox
-      # firefox-devedition-bin
-      sqlite
-      yabai
-      sketchybar
+        # tools
+        # notmuch
+        vim
+        cachix
+        tmux
+        zsh
+        bash
+        git
+        neovim
+        mu
+        # macvim # use vim
+        bitwarden-cli
+        # firefox
+        # firefox-devedition-bin
+        sqlite
+        yabai
+        sketchybar
 
-      # cli tools
-      ripgrep
-      woff2
-      cmus
-      zola
-      git-interactive-rebase-tool
-      htop
-      hub
-      gh
-      # fnm
-      doxygen
-      gmailctl
-      leiningen
-      shellcheck
-      proselint
-      trash-cli
-      delta
-      entr
-      # proxychains-ng # build failed
-      du-dust
-      duf # du
-      jq
-      fd
-      ctags
-      # gitui
-      # httpie # failed to build
-      tealdeer # tldr in rust
-      # procs # ps
-      bottom # btm htop
-      thefuck
-      nix-tree
-      nix-linter
-      curlie # httpie
-      # glances # htop
-      xh # httpie
-      shfmt
-      hunspell
-      ccache
-      # dotenv-linter # failed
-      reattach-to-user-namespace
-      terminal-notifier
-      enchant # ispell
-      w3m
-      starship
-      broot # ranger
-      direnv
-      exa
-      # fasd
-      fzf
-      skim
-      coreutils-full
-      gnused
-      pngquant
-      optipng
-      rbenv
-      jless # json viewer
-      gnupg
-      less
-      zoxide # fasd
-      lazygit
-      # watchexec
-      wget
-      # curl
-      rsync
-      treefmt
-      # grip # markdown preview, failed to build
-      multimarkdown
-      languagetool
-      mujs
-      brotli
-      automake
-      autoconf
-      bat
-      bat-extras.batdiff
-      bat-extras.batgrep
-      bat-extras.batman
-      bat-extras.batwatch
-      bat-extras.prettybat
-      gnutls
-      openssl
-      pinentry
-      pinentry_mac
-      feroxbuster
-      ffmpeg
-      pandoc
-      cmake
-      # goku
-      nix
-      nixfmt
-      nixpkgs-fmt
-      rnix-lsp
-      openssh
-      neofetch
-      parallel
-      # terraform
-      # vagrant
-      pre-commit
-      # espanso # build failed
-      nix-doc
-      gawk
-      flyctl
-      jet
-      sd
-      asdf-vm
-      vmtouch
-      # commitizen
-      graphviz
-      perl534Packages.GitAutofixup # https://github.com/torbiak/git-autofixup
-      git-absorb # https://github.com/tummychow/git-absorb
-      gnumake
-      cmake
-      dtach
-      difftastic
-      # python39Packages.percol # build failed
-      # docker-compose_2
+        # cli tools
+        ripgrep
+        woff2
+        cmus
+        zola
+        git-interactive-rebase-tool
+        htop
+        hub
+        gh
+        # fnm
+        doxygen
+        gmailctl
+        shellcheck
+        proselint
+        trash-cli
+        delta
+        entr
+        # proxychains-ng # build failed
+        du-dust
+        duf # du
+        jq
+        fd
+        ctags
+        # gitui
+        # httpie # failed to build
+        tealdeer # tldr in rust
+        # procs # ps
+        bottom # btm htop
+        thefuck
+        nix-tree
+        nix-linter
+        curlie # httpie
+        # glances # htop
+        xh # httpie
+        shfmt
+        hunspell
+        ccache
+        # dotenv-linter # failed
+        reattach-to-user-namespace
+        terminal-notifier
+        enchant # ispell
+        w3m
+        starship
+        broot # ranger
+        direnv
+        exa
+        # fasd
+        fzf
+        skim
+        coreutils-full
+        gnused
+        pngquant
+        optipng
+        rbenv
+        jless # json viewer
+        gnupg
+        less
+        zoxide # fasd
+        lazygit
+        # watchexec
+        wget
+        # curl
+        rsync
+        treefmt
+        # grip # markdown preview, failed to build
+        multimarkdown
+        languagetool
+        mujs
+        brotli
+        automake
+        autoconf
+        bat
+        bat-extras.batdiff
+        bat-extras.batgrep
+        bat-extras.batman
+        bat-extras.batwatch
+        bat-extras.prettybat
+        gnutls
+        openssl
+        pinentry
+        pinentry_mac
+        feroxbuster
+        ffmpeg
+        pandoc
+        cmake
+        # goku
+        nix
+        nixfmt
+        nixpkgs-fmt
+        rnix-lsp
+        openssh
+        neofetch
+        parallel
+        # terraform
+        # vagrant
+        pre-commit
+        # espanso # build failed
+        nix-doc
+        gawk
+        flyctl
+        jet
+        sd
+        asdf-vm
+        vmtouch
+        # commitizen
+        graphviz
+        perl534Packages.GitAutofixup # https://github.com/torbiak/git-autofixup
+        git-absorb # https://github.com/tummychow/git-absorb
+        gnumake
+        cmake
+        dtach
+        difftastic
+        # python39Packages.percol # build failed
+        # docker-compose_2
 
-      redis
-      # langs
-      tree-sitter
-      nodePackages.expo-cli
-      nodePackages.eslint
-      nodePackages.diagnostic-languageserver
-      nodePackages.typescript
-      nodePackages.eslint_d
-      nodePackages.markdownlint-cli2
-      nodePackages.prettier
-      nodePackages.nodemon
-      nodePackages.serve
-      nodePackages.surge
-      nodePackages.neovim
-      nodePackages.live-server
-      nodePackages.web-ext
-      nodePackages.textlint
-      nodePackages.textlint-rule-write-good
-      nodePackages.typescript-language-server
-      nodePackages.vscode-css-languageserver-bin
-      nodePackages.vscode-html-languageserver-bin
-      nodePackages.vscode-css-languageserver-bin
-      nodePackages.vscode-langservers-extracted
-      nodePackages.vscode-json-languageserver
-      nodePackages.dockerfile-language-server-nodejs
-      nodePackages.bash-language-server
-      nodePackages.yaml-language-server
-      yarn
-      # php
-      lua
-      # luajit
-      stylua
-      # deno
-      # fennel
-      # janet # broken
-      # luarocks
-      # zig # marked broken
-      go
-      # golangci-lint
-      plantuml
-      rustup
-      rust-analyzer
-      (pkgs.ruby.withPackages (ps: with ps; [ rufo solargraph ]))
-      python3
-      ghc
-      # chez
-      sbcl
-      guile
-      roswell
-      clang-tools
-      html-tidy
-      ninja
-      perl
-      # openjdk
+        redis
+        # langs
+        tree-sitter
+        nodePackages.expo-cli
+        nodePackages.eslint
+        nodePackages.diagnostic-languageserver
+        nodePackages.typescript
+        nodePackages.eslint_d
+        nodePackages.markdownlint-cli2
+        nodePackages.prettier
+        nodePackages.nodemon
+        nodePackages.serve
+        nodePackages.surge
+        nodePackages.neovim
+        nodePackages.live-server
+        nodePackages.web-ext
+        nodePackages.textlint
+        nodePackages.textlint-rule-write-good
+        nodePackages.typescript-language-server
+        nodePackages.vscode-css-languageserver-bin
+        nodePackages.vscode-html-languageserver-bin
+        nodePackages.vscode-css-languageserver-bin
+        nodePackages.vscode-langservers-extracted
+        nodePackages.vscode-json-languageserver
+        nodePackages.dockerfile-language-server-nodejs
+        nodePackages.bash-language-server
+        nodePackages.yaml-language-server
+        yarn
+        # php
+        lua
+        # luajit
+        stylua
+        # deno
+        # fennel
+        # janet # broken
+        # luarocks
+        # zig # marked broken
+        go
+        # golangci-lint
+        plantuml
+        rustup
+        rust-analyzer
+        (pkgs.ruby.withPackages (ps: with ps; [ rufo solargraph ]))
 
-      # entertainment
-      youtube-dl
-      yt-dlp
-      # streamlink
-      mpv
-      you-get
-      watchman
+        ghc
+        # chez
+        sbcl
+        guile
+        roswell
+        clang-tools
+        html-tidy
+        ninja
+        perl
+        # openjdk
 
-      # lib
-      # libgccjit
-      zlib
-      rlwrap
-      readline
-      llvm
-      glib # libvterm
-      # texinfo # cllision to pod2texi
-      pkg-config
-      # pcre
-      # pcre2
-      harfbuzz
-      librsvg
-      imagemagick
-      # openjpeg
+        watchman
 
-      # not available
-      # du
-    ];
+        # lib
+        # libgccjit
+        zlib
+        rlwrap
+        readline
+        llvm
+        glib # libvterm
+        # texinfo # cllision to pod2texi
+        pkg-config
+        # pcre
+        # pcre2
+        harfbuzz
+        librsvg
+        imagemagick
+        # openjpeg
+
+        # not available
+        # du
+      ] ++ stables;
     etc = {
       home-manager.source = "${inputs.home-manager}";
       nixpkgs.source = "${pkgs.path}";
