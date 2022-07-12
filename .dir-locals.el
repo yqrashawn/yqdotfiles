309de7ca292bcc43e3b50b8f9f9a2d021d8213af
yqrashawn <namy.19@gmail.com>
2022-07-12 16:10:22 +0800

    chore: misc

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-  ((nil . ((compile-command . "sysdo switch --darwin yqrashawn-intel"))))
+((nil . ((compile-command . "sysdo switch --darwin yqrashawn-intel")))
+ (clojure-mode . ((compile-command . "goku -c ~/.nixpkgs/modules/yqrashawn/home-manager/dotfiles/karabiner.edn"))))
