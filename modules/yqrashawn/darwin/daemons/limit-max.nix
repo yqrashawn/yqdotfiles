31db8389a88b7d0c1322f84100972976d154742c
yqrashawn <namy.19@gmail.com>
2022-09-23 13:43:30 +0800

    fix: maxfiles maxproc

--- modules/yqrashawn/darwin/daemons/limit-max.nix
+++ modules/yqrashawn/darwin/daemons/limit-max.nix
@@ -3,21 +3,45 @@
 {
   launchd.daemons = {
     limit-maxfiles = {
-      script = "/bin/launchctl limit maxfiles 524288 16777216";
-      serviceConfig.RunAtLoad = true;
-      serviceConfig.KeepAlive = false;
+      command = "/bin/launchctl limit maxfiles 524288 16777216";
+      # script = "/bin/launchctl limit maxfiles 524288 16777216";
+      serviceConfig = {
+        # Program = "/bin/launchctl";
+        # ProgramArguments = [ "limit" "maxfiles" "524288" "16777216" ];
+        # ProgramArguments = [ "limit" "maxfiles 524288 16777216" ];
+        RunAtLoad = true;
+        KeepAlive = false;
+        # LaunchOnlyOnce = true;
+        StandardOutPath = "/tmp/nix-custom-log.out.log";
+        StandardErrorPath = "/tmp/nix-custom-log.error.log";
+      };
+
     };
     limit-maxproc = {
-      script = "/bin/launchctl limit maxproc 16704 16704";
-      serviceConfig.RunAtLoad = true;
-      serviceConfig.KeepAlive = false;
+      command = "/bin/launchctl limit maxproc 16704 16704";
+      # script = "/bin/launchctl limit maxproc 16704 16704";
+      serviceConfig = {
+        # Program = "/bin/launchctl";
+        # ProgramArguments = [ "limit" "maxproc" "16704" "16704" ];
+        # ProgramArguments = [ "limit" "maxproc 16704 16704" ];
+        RunAtLoad = true;
+        KeepAlive = false;
+        # LaunchOnlyOnce = true;
+        StandardOutPath = "/tmp/nix-custom-log.out.log";
+        StandardErrorPath = "/tmp/nix-custom-log.error.log";
+      };
     };
     sysctl = {
-      script =
-        "/usr/sbin/sysctl kern.maxfiles=16777216 kern.maxfilesperproc=16704";
-      serviceConfig.RunAtLoad = true;
-      serviceConfig.KeepAlive = false;
-      serviceConfig.LaunchOnlyOnce = true;
+      serviceConfig = {
+        Program = "/usr/sbin/sysctl";
+        ProgramArguments =
+          [ "kern.maxfiles=16777216" "kern.maxfilesperproc=16704" ];
+        RunAtLoad = true;
+        KeepAlive = false;
+        # LaunchOnlyOnce = true;
+        StandardOutPath = "/tmp/nix-custom-log.out.log";
+        StandardErrorPath = "/tmp/nix-custom-log.error.log";
+      };
     };
   };
 }
