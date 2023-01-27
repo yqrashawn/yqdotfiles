567c1d793837355ba0625381f7a55ff57a059856
yqrashawn <namy.19@gmail.com>
2023-01-27 08:52:54 +0800

    chore: misc

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ignored-local-variable-values '((+clojure-use-zprint-formatter)))
  '(org-safe-remote-resources
     '("\\`https://raw\\.githubusercontent\\.com/yqrashawn/org-html-themes/master/org/theme-bigblow\\.setup\\'"))
  '(safe-local-variable-values
