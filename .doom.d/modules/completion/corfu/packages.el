8626a3b86b04dbaef3b062b8be8c0b58016a941f
yqrashawn <namy.19@gmail.com>
2023-02-06 14:54:44 +0800

    fix: pin corfu, compat version issue

--- .doom.d/modules/completion/corfu/packages.el
+++ .doom.d/modules/completion/corfu/packages.el
@@ -1,5 +1,6 @@
 (package! corfu
-  :recipe (:files (:defaults "extensions/*.el")))
+  :recipe (:type git :repo "minad/corfu" :files (:defaults "extensions/*.el"))
+  :pin "5c4f34b68bde51beb7edfa0d1643630358aca02a")
 
 (when (modulep! +icons)
   (package! kind-icon))
