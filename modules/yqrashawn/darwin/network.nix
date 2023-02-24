2c72a999e097bf42e18d7fd753b52b5900140ed6
yqrashawn <namy.19@gmail.com>
2023-02-24 14:20:14 +0800

    feat: dns config

---
+++ modules/yqrashawn/darwin/network.nix
@@ -0,0 +1,27 @@
+{ config, lib, pkgs, ... }:
+
+{
+  networking = {
+    knownNetworkServices = [
+      "Wi-Fi"
+      "Thunderbolt Bridge"
+      "AdGuard"
+      "Tailscale Tunnel"
+      "iPhone USB"
+      "USB 10/100/1000 LAN"
+      # "LG Monitor Controls"
+    ];
+    # search = [ "yqrashawn" ];
+    dns = [
+      "2408:8207:2532:2340:52eb:f6ff:fe34:b910"
+      "192.18.0.2"
+      "100.100.100.100"
+      "117.50.10.10"
+      "52.80.52.52"
+      "119.29.29.29"
+      "223.5.5.5"
+      "223.6.6.6"
+    ];
+  };
+
+}
