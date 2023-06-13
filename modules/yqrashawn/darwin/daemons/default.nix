3a8459fd6fdf7f7afb0e48817b0551bc88512c67
yqrashawn <namy.19@gmail.com>
2023-06-13 15:27:27 +0800

    feat: disable tailscaled

--- modules/yqrashawn/darwin/daemons/default.nix
+++ modules/yqrashawn/darwin/daemons/default.nix
@@ -1,4 +1,8 @@
 { config, lib, pkgs, ... }: {
-  imports =
-    [ ./set-path.nix ./limit-max.nix ./tailscale.nix ./adguard-home.nix ];
+  imports = [
+    ./set-path.nix
+    ./limit-max.nix
+    # ./tailscale.nix
+    ./adguard-home.nix
+  ];
 }
