6df13d63d79eadfd547ac478725664e6a3a4c9d0
yqrashawn <namy.19@gmail.com>
2024-01-24 10:28:43 +0800

    refactor: cleanup

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -3,8 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(safe-local-variable-values
-   '((cider-clojure-cli-aliases . ":jvm-base")
+   '((apheleia-formatter . zprint)
+     (cider-clojure-cli-aliases . ":jvm-base")
      (eval add-hook 'before-save-hook
       (lambda nil
         (if
