d06feb9642bfc773ca518a10c23e76c57f64cb7b
yqrashawn <namy.19@gmail.com>
2024-02-17 15:06:34 +0800

    chore: misc

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -202,7 +202,7 @@ See the docstrings of `defalias' and `make-obsolete' for more details."
        editorconfig        ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
-       gist                ; interacting with github gists
+       ;; gist                ; interacting with github gists
        (lookup +dectionary +docsets)              ; navigate your code and its documentation
        lsp
        (magit +forge)      ; a git porcelain for Emacs
