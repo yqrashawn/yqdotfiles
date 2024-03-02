35f22000f3458b03ed017062ed7caf633ba670f6
yqrashawn <namy.19@gmail.com>
2024-03-02 08:54:06 +0800

    chore: disable vterm

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -186,7 +186,7 @@ See the docstrings of `defalias' and `make-obsolete' for more details."
        eshell            ; the elisp shell that works everywhere
        ;;shell           ; simple shell REPL for Emacs
        ;;term            ; basic terminal emulator for Emacs
-       vterm             ; the best terminal emulation in Emacs
+       ;; vterm             ; the best terminal emulation in Emacs
 
        :checkers
        (syntax +childframe); tasing you for every semicolon you forget
