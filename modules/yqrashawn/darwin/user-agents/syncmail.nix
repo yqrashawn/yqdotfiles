c9156652deb251956b53b2311092ecd8a9159bd4
yqrashawn <namy.19@gmail.com>
2024-10-09 09:55:13 +0800

    fix: syncmail

--- modules/yqrashawn/darwin/user-agents/syncmail.nix
+++ modules/yqrashawn/darwin/user-agents/syncmail.nix
@@ -1,9 +1,13 @@
 { config, lib, pkgs, ... }: {
   launchd.user.agents.syncmail = {
+    path = [ config.environment.systemPath ];
     serviceConfig = {
       # Disabled = true;
-      Program = "${pkgs.babashka}/bin/bb";
-      ProgramArguments = [ "-f" "${config.user.home}/local/bin/syncmail" ];
+      Program = "${config.user.home}/.asdf/shims/bb";
+      ProgramArguments = [
+        "--file"
+        "${config.user.home}/.nixpkgs/modules/yqrashawn/home-manager/dotfiles/local-bins/syncmail"
+      ];
       StandardErrorPath = "/tmp/syncmail.error.log";
       StandardOutPath = "/tmp/syncmail.out.log";
       RunAtLoad = true;
