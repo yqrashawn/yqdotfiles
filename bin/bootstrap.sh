8b3cbc501e729aa76fb44121ce6f56e93032f6ad
yqrashawn <namy.19@gmail.com>
2024-12-05 22:01:34 +0800

    fix: bootstrap.sh

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 # yqrashawn is the bootstrap target
-nix --extra-experimental-features "nix-command flakes" develop -c sysdo bootstrap --darwin yqrashawn
-# nix --extra-experimental-features "nix-command flakes" develop -c sysdo bootstrap --home-manager yqrashawn
+nix --extra-experimental-features "nix-command flakes" develop -c sysdo bootstrap --darwin "$(id -un)"
+# nix --extra-experimental-features "nix-command flakes" develop -c sysdo bootstrap --home-manager "$(id -un)"
