e6ffded3442fccde33a0c40e829e102e21c44b48
yqrashawn <namy.19@gmail.com>
2025-07-17 09:30:14 +0800

    chore: restart miniser container on macmini

---
+++ modules/yqrashawn/darwin/user-agents/restart-miniser-container.nix
@@ -0,0 +1,29 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  launchd.user.agents.restart-miniser-container = {
+    serviceConfig = {
+      Label = "com.yqrashawn.restart-miniser-container";
+      ProgramArguments = [
+        "/bin/sh"
+        "-c"
+        ''
+          /usr/local/bin/docker restart miniser
+        ''
+      ];
+      RunAtLoad = true;
+      KeepAlive = true;
+      StartCalendarInterval = {
+        Hour = 4;
+        Minute = 1;
+      };
+      StandardErrorPath = "/tmp/restart-miniser-stderr.log";
+      StandardOutPath = "/tmp/restart-miniser-stdout.log";
+    };
+  };
+}
--- modules/yqrashawn/mini.nix
+++ modules/yqrashawn/mini.nix
@@ -1,7 +1,14 @@
-{ config, lib, pkgs, ... }: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
   imports = [
     ./darwin/daemons/tailscale.nix
     ./darwin/daemons/kill-bootpd.nix
     ./darwin/daemons/adguard-home-with-proxy.nix
+    ./darwin/user-agents/restart-miniser-container.nix
   ];
 }
