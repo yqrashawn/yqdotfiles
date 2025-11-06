98dc9a8777c1b6bcaded0854daf349b67a4410cb
yqrashawn <namy.19@gmail.com>
2025-11-07 06:54:18 +0800

    feat: gptel tool, lint project

--- .doom.d/gptel-tools/lint.el
+++ .doom.d/gptel-tools/lint.el
@@ -1,6 +1,6 @@
 ;;; lint.el --- GPTEL lint tool -*- lexical-binding: t; -*-
 ;; Version: 0.1
-;; Package-Requires: ((emacs "25.1"))
+;; Package-Requires: ((emacs "28.1"))
 ;; Homepage: https://github.com/yqrashawn/gptel-tools
 ;; Keywords: tools, lint
 
@@ -31,12 +31,16 @@ Optional OFFSET and LIMIT for paging diagnostics (default offset=0, limit=50)."
       (cond
        ((and (bound-and-true-p lsp-mode)
              (fboundp 'lsp-diagnostics))
-        (let ((lsp-diags (ignore-errors (lsp-diagnostics t))))
-          (when lsp-diags
+        (let* ((file-path (buffer-file-name buf))
+               (lsp-diags (ignore-errors (lsp-diagnostics t)))
+               (file-diags (when (and lsp-diags file-path)
+                             (gethash file-path lsp-diags))))
+          (when file-diags
             (setq results
                   (append results
-                          (list (list :source "lsp" :diagnostics lsp-diags)))))))
-       ((and (featurep 'flycheck) (bound-and-true-p flycheck-mode))
+                          (list (list :source "lsp" :diagnostics file-diags)))))))
+       ((and (featurep 'flycheck)
+             (bound-and-true-p flycheck-mode))
         (let ((errs (and (boundp 'flycheck-current-errors)
                          flycheck-current-errors)))
           (when errs
@@ -58,9 +62,19 @@ Optional OFFSET and LIMIT for paging diagnostics (default offset=0, limit=50)."
                     (lambda (item)
                       (let ((src (plist-get item :source))
                             (diags (plist-get item :diagnostics)))
-                        (ht-map (lambda (k diag)
-                                  (cons :source (cons k diag)))
-                                diags)))
+                        (if (hash-table-p diags)
+                            ;; lsp-diagnostics returns hash table per file
+                            (let ((file-diags '()))
+                              (maphash (lambda (_file diag-list)
+                                         (setq file-diags (append file-diags diag-list)))
+                                       diags)
+                              (mapcar (lambda (diag)
+                                        (cons :source (cons src diag)))
+                                      file-diags))
+                          ;; other sources return list directly
+                          (mapcar (lambda (diag)
+                                    (cons :source (cons src diag)))
+                                  diags))))
                     results)))
            (total (length flat-list))
            (start (or offset 0))
@@ -70,6 +84,77 @@ Optional OFFSET and LIMIT for paging diagnostics (default offset=0, limit=50)."
 
 (comment
   (gptelt-lint-buffer (current-buffer)))
+
+;;; lint project
+(defun gptelt-lint-project (&optional offset limit)
+  "Get all linter diagnostics for the entire project.
+
+Returns diagnostics from user's chosen linters across all project files.
+Use this to get an overview of all issues in the project.
+Optional OFFSET and LIMIT for paging diagnostics (default offset=0, limit=50)."
+  (let ((results '()))
+    (cond
+     ;; LSP: Get project-wide diagnostics
+     ((and (bound-and-true-p lsp-mode)
+           (fboundp 'lsp-diagnostics))
+      (let ((lsp-diags (ignore-errors (lsp-diagnostics t))))
+        (when lsp-diags
+          (setq results
+                (append results
+                        (list (list :source "lsp" :diagnostics lsp-diags)))))))
+     ;; Flymake: Get project-wide diagnostics
+     ((and (featurep 'flymake)
+           (fboundp 'flymake--project-diagnostics))
+      (when-let ((project (project-current)))
+        (let ((diags (ignore-errors (flymake--project-diagnostics project))))
+          (when diags
+            (setq results (append results
+                                  (list (list :source "flymake" :diagnostics diags))))))))
+     ;; Flycheck: Collect from all project buffers
+     ((featurep 'flycheck)
+      (when-let ((project (project-current)))
+        (let ((all-errs '()))
+          (dolist (buf (buffer-list))
+            (with-current-buffer buf
+              (when (and (bound-and-true-p flycheck-mode)
+                         (boundp 'flycheck-current-errors)
+                         flycheck-current-errors
+                         (project-current)
+                         (equal (project-root (project-current))
+                                (project-root project)))
+                (setq all-errs (append all-errs flycheck-current-errors)))))
+          (when all-errs
+            (setq results (append results
+                                  (list (list :source "flycheck" :diagnostics all-errs)))))))))
+    ;; Flatten and page results
+    (let* ((flat-list
+            (apply #'append
+                   (mapcar
+                    (lambda (item)
+                      (let ((src (plist-get item :source))
+                            (diags (plist-get item :diagnostics)))
+                        (if (hash-table-p diags)
+                            ;; lsp-diagnostics returns hash table with all files
+                            (let ((file-diags '()))
+                              (maphash (lambda (_file diag-list)
+                                         (setq file-diags (append file-diags diag-list)))
+                                       diags)
+                              (mapcar (lambda (diag)
+                                        (cons :source (cons src diag)))
+                                      file-diags))
+                          ;; other sources return list directly
+                          (mapcar (lambda (diag)
+                                    (cons :source (cons src diag)))
+                                  diags))))
+                    results)))
+           (total (length flat-list))
+           (start (or offset 0))
+           (lim (or limit 50))
+           (paged (seq-subseq flat-list start (min total (+ start lim)))))
+      (list :total total :offset start :limit lim :diagnostics paged))))
+
+(comment
+  (gptelt-lint-project))
 
 ;;; lint file
 (defun gptelt-lint-file (file-path &optional offset limit)
@@ -140,6 +225,30 @@ Check this at the end of each task to ensure no errors were introduced."
             :optional t))
    :category "lint"
    :confirm nil
+   :include t)
+  (gptelt-make-tool
+   :name "lint_project"
+   :function #'gptelt-lint-project
+   :description "Get all diagnostics from user-configured linters for the entire project.
+
+Returns project-wide diagnostics from:
+- LSP: All project diagnostics from LSP workspace
+- Flymake: Project-wide diagnostics via flymake--project-diagnostics
+- Flycheck: Collected from all project buffers with flycheck-mode enabled
+
+Use this to get an overview of all issues across the project.
+Optional OFFSET and LIMIT for paging diagnostics (default offset=0, limit=50)."
+   :args '((:name "offset"
+            :type integer
+            :description "Start from this diagnostic (default 0)"
+            :optional t)
+           (:name "limit"
+            :type integer
+            :description "Number of diagnostics to return (default 50)"
+            :optional t))
+   :category "lint"
+   :confirm nil
    :include t))
 
+(provide 'lint)
 ;;; lint.el ends here
