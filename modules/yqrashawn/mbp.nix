873267c1f29322dd9f405fd4da01f2ad4059dd9c
yqrashawn <namy.19@gmail.com>
2026-01-04 11:39:07 +0800

    chore: misc

--- modules/yqrashawn/mbp.nix
+++ modules/yqrashawn/mbp.nix
@@ -1,8 +1,14 @@
-{ config, lib, pkgs, ... }: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
   imports = [
     ./darwin/daemons/tailscale.nix
-    ./darwin/user-agents/miniser-local-jar.nix
+    # ./darwin/user-agents/miniser-local-jar.nix
     ./darwin/user-agents/pextser.nix
-    ./darwin/daemons/adguard-home-with-proxy.nix
+    # ./darwin/daemons/adguard-home-with-proxy.nix
   ];
 }
